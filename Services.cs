7b4153b71949da3bf9319fc2325b281a7964d2a2
Daylend87 <macha05112011@gmail.com>
2025-04-16 23:03:09 +0500

    Services.cs

--- Services.cs
+++ Services.cs
@@ -1,16 +1,23 @@
-﻿using TriangleApp.Models;
+using TriangleApp.Models;
 
 namespace TriangleApp.Services
 {
     public class TriangleValidator
     {
-        public static bool Exists(Triangle triangle)
+        public bool Exists(Triangle triangle)
         {
-            double a = triangle.SideA;
-            double b = triangle.SideB;
-            double c = triangle.SideC;
+            if (triangle == null)
+                throw new ArgumentNullException("Треугольник не может быть пустым.");
+
+            double a = triangle.A;
+            double b = triangle.B;
+            double c = triangle.C;
+
+            if (a == 0 || b == 0 || c == 0)
+                return false;
 
             return (a + b > c) && (a + c > b) && (b + c > a);
         }
     }
 }
+
